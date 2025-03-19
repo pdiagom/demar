67f050efaeaf88c88dab9f409dd1b1c0d667e0de
pdiagom <pabloalosno@gmail.com>
2025-03-19 09:39:25 +0100

    Arreglo acceso articulos, fallo en creacion

--- demarapp/views.py
+++ demarapp/views.py
@@ -42,38 +42,10 @@ class LoginView(generics.GenericAPIView):
 # ARTÍCULOS (Article)
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.AllowAny]  # Permitir acceso sin autenticación
     serializer_class = ArticleSerializer
-    
-    def list(self, request):
-        articles = self.get_queryset()
-        serializer = self.get_serializer(articles, many=True)
-        return Response(serializer.data)
-    
-    def retrieve(self, request, pk=None):
-        article = get_object_or_404(self.queryset, pk=pk)
-        serializer = self.get_serializer(article)
-        return Response(serializer.data)
+  
 
-    def create(self, request):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(user=request.user)  # Asigna el usuario actual
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, pk=None):
-        article = get_object_or_404(self.queryset, pk=pk)
-        serializer = self.get_serializer(article, data=request.data, partial=True)  # Actualización parcial
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        article = get_object_or_404(self.queryset, pk=pk)
-        article.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 # CATEGORÍAS (Category)
 class CategoryViewSet(viewsets.ModelViewSet):
--- demarfrontend/src/services/categoryService.js
+++ demarfrontend/src/services/categoryService.js
@@ -2,14 +2,17 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:8000/demar/';
 
+const handleResponse = (response) => {
+    if (!response.ok) {
+        throw new Error("Error en la respuesta de la API");
+    }
+    return response.json(); // Devuelve la respuesta convertida a JSON
+};
+
 export const getCategories = async () => {
     try {
-        const token = localStorage.getItem('token'); // Obtiene el token del almacenamiento local
         const response = await fetch(`${API_URL}categories/`, {
             method: "GET",
-            headers: {
-                Authorization: `Bearer ${token}`, // Agrega el token en el encabezado
-            },
         });
         return await handleResponse(response); // Maneja la respuesta
     } catch (error) {
