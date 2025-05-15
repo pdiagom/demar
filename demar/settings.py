ef86d615c870e24becfa882cb8bd6117a1ad06fa
pdiagom <pabloalosno@gmail.com>
2025-05-15 09:26:18 +0200

    Vuelta de settings al origen

--- demar/settings.py
+++ demar/settings.py
@@ -88,11 +88,11 @@ CORS_ALLOW_ALL_ORIGINS = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'demar_zjx1',
+        'NAME': 'demar',
         'USER': 'root',
-        'PASSWORD': 'qmAhyCMZhNahivbuM6jQzekOY8CkE72y',
-        'HOST': 'dpg-d0ip9lbe5dus739rb0ug-a',  # O la IP del servidor de MySQL
-        'PORT': '5432',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  # O la IP del servidor de MySQL
+        'PORT': '3306',
         'OPTIONS': {
             'charset': 'utf8mb4',
         },
