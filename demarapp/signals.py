5d920eb674b0e1c264ca10d7c75b8ae7e6d4f2fd
pdiagom <pabloalosno@gmail.com>
2025-05-19 17:58:43 +0200

    Comienzo arreglo reset password

--- demarapp/signals.py
+++ demarapp/signals.py
@@ -6,7 +6,7 @@ from django_rest_passwordreset.signals import reset_password_token_created
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
     # Construye la URL de restablecimiento de contraseña
-    reset_password_url = "http://localhost:5173/password-reset-confirm/?token={}".format(reset_password_token.key)
+    reset_password_url = "https://demar-gray.vercel.app/password-reset-confirm/?token={}".format(reset_password_token.key)
 
 
     email_plaintext_message = "Para restablecer tu contraseña, haz clic en el siguiente enlace:\n{}".format(reset_password_url)
