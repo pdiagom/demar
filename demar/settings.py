5d28deccbe45c00edf210004605d11307fccf5e6
pdiagom <pabloalosno@gmail.com>
2025-05-19 16:31:19 +0200

    Subida de imagenes a AWS

--- demar/settings.py
+++ demar/settings.py
@@ -199,7 +199,9 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'demar-media'  # usa tu nombre de bucket
 AWS_S3_REGION_NAME = 'eu-north-1'  # cambia si usaste otra región
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.eu-north-1.amazonaws.com'
 AWS_QUERYSTRING_AUTH = False  # Hace que las URLs no tengan tokens temporales
 
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+
+
