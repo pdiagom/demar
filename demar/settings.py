a91e2316a4cbbae7b4ea23ad8e3b377ba87b7bb8
pdiagom <pabloalosno@gmail.com>
2025-05-19 12:15:27 +0200

    Actualización settings.py

--- demar/settings.py
+++ demar/settings.py
@@ -170,8 +170,13 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
+STATIC_URL = '/static/'
+
+# Añade esta línea:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
