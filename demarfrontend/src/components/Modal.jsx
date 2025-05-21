d9db23443df7bac63465e2ec5fc34afdb34bc22c
pdiagom <pabloalosno@gmail.com>
2025-05-21 16:29:15 +0200

    Ajuste boton close modal

--- demarfrontend/src/components/Modal.jsx
+++ demarfrontend/src/components/Modal.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import '../styles/modal.css'; // Asegúrate de crear este archivo CSS
+import React from "react";
+import "../styles/modal.css"; // Asegúrate de crear este archivo CSS
 
 const Modal = ({ show, onClose, children }) => {
-    if (!show) {
-        return null;
-    }
+  if (!show) {
+    return null;
+  }
 
-    return (
-        <div className="modal-backdrop">
-            <div className="modal-content">
-                <button className="modal-close" onClick={onClose}>
-                    &times;
-                </button>
-                {children}
-            </div>
-        </div>
-    );
+  return (
+    <div className="modal-backdrop">
+      <div className="modal-content">
+        <button className="modal-close btn-close" onClick={onClose}>
+          &times;
+        </button>
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
