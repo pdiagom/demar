ed60db7b361b9f97a0422072961f766283e7ad8f
pdiagom <pabloalosno@gmail.com>
2025-05-26 14:05:02 +0200

    Prueba scroll carrito 2

--- demarfrontend/src/components/ScrollToCartButton.jsx
+++ demarfrontend/src/components/ScrollToCartButton.jsx
@@ -1,6 +1,6 @@
 // src/components/ScrollToCartButton.jsx
 import React from 'react';
-
+import carrito from '../../../media/media/carrito.png'; 
 const ScrollToCartButton = () => {
   const scrollToCart = () => {
     const cartElement = document.querySelector('.cart-container');
@@ -11,7 +11,7 @@ const ScrollToCartButton = () => {
 
   return (
     <button className="scroll-to-cart-button" onClick={scrollToCart}>
-      Ir al Carrito
+      <img src={carrito} alt="carrito" />
     </button>
   );
 };
